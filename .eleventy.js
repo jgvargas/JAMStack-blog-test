6e92f342189c0585d61709062ceb482a7b623fcf
Juan G. Vargas Jr <18316297+jgvargas@users.noreply.github.com>
2022-04-24 11:42:05 -0500

    11ty the static pages

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+// Where are the source files, where are you working from, where are public facing files
+/*  
+    input: "src"
+    Output doesn't need to be added but will default to
+        output: "_site"
+
+    
+*/
+
+const {DateTime} = require('luxon')
+
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./src/style.css')
+    eleventyConfig.addPassthroughCopy('./src/assets')
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    })
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+}
